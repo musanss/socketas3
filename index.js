e4451f2f726b5d5617084505bf477bd564d23a77
musanss <39636638+musanss@users.noreply.github.com>
2018-10-14 22:01:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var WebSocketServer = require("ws").Server
 var http = require("http")
+
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
@@ -7,12 +8,19 @@ var port = process.env.PORT || 5000
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
+var io = require('socket.io').listen(server, {transports:['flashsocket', 'websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']})
 server.listen(port)
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('WelcomeMsg', 'hello world');
+});
+
 
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
+
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
