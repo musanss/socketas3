f2a4d34aa68430ef7061daee19344781dec07304
musanss <39636638+musanss@users.noreply.github.com>
2018-10-14 22:25:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const server = require('http').createServer();
+const Server = require('socket.io');
+const PORT   = 3000;
+var app = express();
+const server = require('http').Server();
 
-const io = require('socket.io')(server, {
-  transports:['flashsocket', 'websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'],
-  path: '/test',
-  serveClient: false,
-  pingInterval: 10000,
-  pingTimeout: 5000,
-  cookie: false
+const io = Server(PORT);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000);
+server.listen(PORT); // PORT is free to use
+
+io = Server(server);
