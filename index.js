0b02b7ce34f73d42b7696e076ef445e703a3956a
musanss <39636638+musanss@users.noreply.github.com>
2018-10-14 22:16:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const
-    io = require("socket.io"),
-    server = io.listen(8000);
+const server = require('http').createServer();
 
-let
-    sequenceNumberByClient = new Map();
-
-// event fired every time a new client connects:
-server.on("connection", (socket) => {
-    console.info(`Client connected [id=${socket.id}]`);
-    // initialize this client's sequence number
-    sequenceNumberByClient.set(socket, 1);
-
-    // when socket disconnects, remove it from the list:
-    socket.on("disconnect", () => {
-        sequenceNumberByClient.delete(socket);
-        console.info(`Client gone [id=${socket.id}]`);
-    });
+const io = require('socket.io')(server, {
+  transports:['flashsocket', 'websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'],
+  path: '/test',
+  serveClient: false,
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
 });
 
-// sends each client its current sequence number
-/*setInterval(() => {
-    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-        client.emit("seq-num", sequenceNumber);
-        sequenceNumberByClient.set(client, sequenceNumber + 1);
-    }
-}, 1000);*/
+server.listen(3000);
